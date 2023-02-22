12a296871dceca21c99ee286e3913b8499aadc54
Oscar Rodriguez <oscar.rodriguez2@asurion.com>
2023-02-22 16:51:57 -0600

    Testing images.

---
+++ src/pages/controls.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Controls() {
+    return (
+        <>
+            <main>
+                Controls
+            </main>
+        </>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import React from 'react';
+import styles from '../styles/Home.module.css';
+import {Button} from "@mui/material";
 
 export default function Home() {
   return (
@@ -9,7 +10,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        Hello
+          <div className={styles.headerContainer}>
+              <>asurion</>
+              <>rental +</>
+              <Button variant={'contained'} className={styles.asurionButton}>Hello</Button>
+          </div>
+
+          <div className={styles.headerContainer}>
+              <img src={'./dektop_hero_image.png'}/>
+              <img src={'./mobile_desktop_image.png'}/>
+          </div>
       </main>
     </>
   );
